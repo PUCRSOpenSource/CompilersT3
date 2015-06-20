26fbc4fe1f49418c644301f32193cf5b97b82ad8
Matthias Nunes <matthiasnunes@gmail.com>
2015-06-20 17:16:16 -0300

    Adiciona programas errados

---
+++ Trabalho/fatorialErrado.pas
@@ -0,0 +1,30 @@
+{ ----------------------------------------- }
+{ calcula o fatorial de um numero informado }
+{ ----------------------------------------- }
+
+program fatorial;
+
+  var
+    num : integer;
+    cont, result : boolean;
+
+
+  begin
+    writeln( "Informe um numero: ");
+    readln( num);
+ 
+    if num > 12 then 
+       writeln( "Numero muito grande!!!")
+    else begin
+
+           cont := 1;
+           result := 1;
+
+           while ( cont < num ) do begin
+              result := result * cont;
+              cont := cont + 1;
+           end;
+
+           writeln( "Valor do fatorial: ", result);
+       end
+  end.
